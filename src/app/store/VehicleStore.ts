4c82113eb788abbaa662860c01944b9d39da2521
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-11-09 16:06:03 +0200

    fixed code

--- src/app/store/VehicleStore.ts
+++ src/app/store/VehicleStore.ts
@@ -1,14 +1,12 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import { IVehicleMake } from "@/app/interfaces/IVehicleMake.interface";
 import VehicleMakesService from "@/app/services/VehicleMakesService";
-import {IVehicleInfo} from "@/app/interfaces/IVehicleInfo.interface";
 
 export class VehicleStore {
   vehicleMakes: IVehicleMake[] = [];
   vehicleMakeId: number | null = null;
   vehicleMakeName: string = "";
   vehicleModelYear: number | null = null;
-  vehicleInfo: IVehicleInfo = {} as IVehicleInfo;
 
   constructor() {
     makeAutoObservable(this);
@@ -39,16 +37,5 @@ export class VehicleStore {
       (make) => make.MakeName === this.vehicleMakeName,
     );
     this.vehicleMakeId = selectedMake ? selectedMake.MakeId : null;
-  }
-
-  async fetchVehicleInfo(makeId: string, year: string) {
-    try {
-      const response = await VehicleMakesService.getVehicleInfo(makeId, year);
-      runInAction(() => {
-        this.vehicleInfo = response.data.Results;
-      });
-    } catch (error) {
-      console.error("Failed to fetch vehicle information:", error);
-    }
   }
 }
